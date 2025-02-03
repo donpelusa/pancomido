9b6deaf19fb6742539b8bf73fbc2d6ff2da629a3
Alvaro Cortés Opazo <92953141+donpelusa@users.noreply.github.com>
2025-02-03 08:50:10 -0300

    Update Credits.jsx

--- src/components/Credits.jsx
+++ src/components/Credits.jsx
@@ -1,7 +1,7 @@
 export const Credits = () => {
   return (
     <div className="bg-[#111a28] text-white text-center text-xs p-1 font-mono">
-      <p className="font-[Fira_Code]">
+      <p className="font-[Fira_Sans]">
         Desarrollado por{" "}
         <a
           href="https://www.alvarocortes.cl"
