50dedc8227e741ab6d6c18f2f75c29d5772dbc71
Alvaro Cortés Opazo <alvaro.cortes@pyaing.local>
2025-02-13 09:37:41 -0300

    Test monorepo en Vercel 2

--- api/src/server.js
+++ api/src/server.js
@@ -23,7 +23,7 @@ app.use((req, res, next) => {
 });
 
 // Rutas de la API
-app.use(routes);
+app.use('/api', routes);
 
 // Endpoint para la documentación Swagger
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
